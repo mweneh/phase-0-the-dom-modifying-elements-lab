f7bfd1309d1cc16356e378e37182bffbacf6f20c
David Ongaro <davidongaroh@gmil.com>
2022-06-11 15:25:10 +0300

    commit tree e1bf507eddc61647f6f98508e520b7d78f558a40

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ document.querySelector("main#main").remove();
 
 
 const newHeader= document.createElement("h1")
-newHeader.id="victory"
+newHeader.id='victory'
 
 newHeader.textContent="is the champion";
